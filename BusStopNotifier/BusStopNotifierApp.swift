9321a08003b47d7e86e9ab556e92b9d00680ff6b
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-08-04 10:24:01 +0500

    Project initialization

---
+++ BusStopNotifier/BusStopNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusStopNotifierApp.swift
+//  BusStopNotifier
+//
+//  Created by Dias Yerlan on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BusStopNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusStopNotifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BusStopNotifier
+//
+//  Created by Dias Yerlan on 04.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
