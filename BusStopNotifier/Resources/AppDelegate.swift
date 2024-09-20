0647ec2e798729f0609049109ab8e9f2cb12eab2
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-09-21 01:35:52 +0500

    .:

--- BusStopNotifier/Resources/AppDelegate.swift
+++ BusStopNotifier/Resources/AppDelegate.swift
@@ -11,8 +11,8 @@ import GooglePlaces
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyBn01OK7-X-171PUdzQiKl09rpN5Zn6pq8")
-        GMSPlacesClient.provideAPIKey("AIzaSyBn01OK7-X-171PUdzQiKl09rpN5Zn6pq8")
+        GMSServices.provideAPIKey(mapsKey)
+        GMSPlacesClient.provideAPIKey(placesKey)
         return true
     }
 }
---
+++ BusStopNotifier/apiKeys.swift
@@ -0,0 +1,11 @@
+//
+//  apiKeys.swift
+//  BusStopNotifier
+//
+//  Created by Dias Yerlan on 21.09.2024.
+//
+
+import Foundation
+
+let placesKey = "AIzaSyBn01OK7-X-171PUdzQiKl09rpN5Zn6pq8"
+let mapsKey = "AIzaSyBn01OK7-X-171PUdzQiKl09rpN5Zn6pq8"
